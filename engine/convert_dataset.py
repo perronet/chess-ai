e2686dc5f0efc146be3f9228b33542e2026cfe47
Marco Perronet <perronet.marco@gmail.com>
2023-02-12 11:55:45 +0100

    Load vectorized dataset

--- engine/convert_dataset.py
+++ engine/convert_dataset.py
@@ -3,12 +3,13 @@ import parse
 import os
 import pandas as pd
 
-CHUNK_SIZE = 100_000
+CHUNK_SIZE = 50_000
+N_ROWS = 1_000_000
 
 if __name__ == "__main__":
     out_path = setup.DATASET_DIR+"converted.csv"
 
-    for i, df_chunk in enumerate(pd.read_csv(setup.DATASET_DIR+setup.DATASET, chunksize=CHUNK_SIZE)):
+    for i, df_chunk in enumerate(pd.read_csv(setup.DATASET_DIR+setup.DATASET, chunksize=CHUNK_SIZE, nrows=N_ROWS)):
         features = [f"f_{str(x)}" for x in range(1, setup.N_FEATURES+1)]
         df_converted = pd.DataFrame(df_chunk["FEN"].apply(lambda fen_str: parse.fen_to_vector(fen_str)).to_list(), columns=features)
         df_converted["label"] = df_chunk.apply(lambda row: parse.convert_stockfish_eval(row.FEN, row.Evaluation), axis=1).reset_index(drop=True)
